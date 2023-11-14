0ccf5d6bf6adc8d04a612da270b57bc578790dfc
Levin <llmar1000000@gmail.com>
2023-11-13 20:13:07 -0500

    Add extra notes in help; minor modifications.

--- src/js/main.ts
+++ src/js/main.ts
@@ -80,7 +80,7 @@ function newConjugation() {
     const verbIndex = Math.floor(Math.random() * csv.length);
     const subjects = firstLine.slice(2);
     const subjectIndex = Math.floor(Math.random() * 6);
-    // const extraNotes = csv[verbIndex][2];
+    const extraNotes = csv[verbIndex][9];
     $("#conj")[0].innerHTML = subjects[subjectIndex] + " " + csv[verbIndex][0];
 
     if (checkDef.checked) {
@@ -94,7 +94,7 @@ function newConjugation() {
 
     $("#defAnswer")[0].innerHTML = defAnswer;
     $("#conjAnswer")[0].innerHTML = conjAnswer;
-    // $("#extra")[0].innerHTML = extraNotes;
+    $("#extra")[0].innerHTML = extraNotes;
 }
 
 conj.addEventListener("keyup", function (event) {
