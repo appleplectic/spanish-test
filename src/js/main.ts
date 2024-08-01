f583448a73a7179e99aa4f0858cbe36c00042bb0
Levin Ma <79768156+appleplectic@users.noreply.github.com>
2024-07-31 20:46:00 -0400

    Update main.ts

--- src/js/main.ts
+++ src/js/main.ts
@@ -273,7 +273,7 @@ ${splitted[1]} (${randomArr[0]}/${randomArr[1]})`;
                     sentence.innerHTML = "";
                 }
             } else {
-                sentence.innerHTML = "Errored. Try waiting a few minutes before pressing again; if the error recurs, contact Levin or donate!";
+                sentence.innerHTML = "Errored. Try waiting a few minutes before pressing again.";
             }
         });
     return "";
